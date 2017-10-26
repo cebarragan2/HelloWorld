b9e3eaacf8ac60ea442e75b08ba87e6a5a16f3e7
cebarragan2 <crismh_ldu@hotmail.com>
2017-10-25 22:57:49 -0500

    v2.0TheChicken

--- Chicken.java
+++ Chicken.java
@@ -1,8 +1,41 @@
 public class Chicken
 {
-	public static void main(String[] args) {
-		System.out.print("Hello Cristian");
+	
+		//atributos
+		private String name;
+		private String color;
+		private int age;
+		//metodos
+		public void initializeData (String name, String color, int age)
+		{
+			this.name=name;
+			this.color=color;
+			this.age=age;
+			
+		}
+		
+		public void layAnEgg()
+		{
+			System.out.println ("the Chicken " + name + " lays an egg");
+		}
+		public void pooped()
+		{
+			System.out.println ("the Chicken "+ name + " pooped");
+		}
+		public void drink ()
+		{
+			System.out.println ("the Chicken " + name + " drank");
+		}
+		
+		
+		
+		public void print ()
+		{
+			System.out.println("the name of the chicken is  "+ name + " color "+color+ "  the age is "+age);
+		}
+		
+		
 
-	}
+	
 
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,32 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.print("Hello Cristian");
+		System.out.println("Hello Cristian");
+		Chicken chicken=new Chicken (); 
+		chicken.initializeData("Lucy","brown",2);
+		chicken.print();
+		chicken.layAnEgg();
+		chicken.pooped();
+		chicken.drink();
+		
+		Chicken chicken1=new Chicken(); 
+		chicken1.initializeData("Confy","yellow",1);
+		chicken1.print();
+		chicken1.layAnEgg();
+		
+		
+		Chicken chicken2=new Chicken(); 
+		chicken2.initializeData("Lupe","black",3);
+		chicken2.print();
+		chicken2.pooped();
+		
+		
+		Chicken chicken3=new Chicken(); 
+		chicken3.initializeData("Maggy","orange",3);
+		chicken3.print();
+		chicken3.drink();
+		
+	
 
 	}
 
